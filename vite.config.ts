f4febbe37c6d3489512dfa330faf005ba5d5f698
Benjamin Nechicattu <benjaminnechicattu@gmail.com>
2025-03-23 22:54:56 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+// Change this to your GitHub repo name
+const repoName = "portfolio";
+
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? `/${repoName}/` : "/",  // <-- ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' ? componentTagger() : null,
+    mode === "development" ? componentTagger() : null,
   ].filter(Boolean),
   resolve: {
     alias: {
