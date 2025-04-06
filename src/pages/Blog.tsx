dd16a57f1641e9d424be755c9403d0b4603404f5
Benjamin Nechicattu <benjaminnechicattu@gmail.com>
2025-04-06 14:36:22 +0530

    updated the blog page

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 import NavBar from '@/components/NavBar';
 import blogs from '@/data/blogs/blogs';
 import BlogCard from '@/components/BlogCard';
 
 const Blog = () => {
+  const navigate = useNavigate();
   const [blogData, setBlogData] = useState([]);
 
   useEffect(() => {
@@ -24,9 +26,24 @@ const Blog = () => {
     <div className="min-h-screen bg-background text-foreground">
       <NavBar />
       <div className="section-container">
+        <div className="flex items-baseline mb-4 space-x-2">
+          <button 
+            className="text-3xl text-black hover:underline" 
+            onClick={() => navigate('/')}
+          >
+            &larr;
+          </button>
+          <nav className="text-sm text-muted-foreground">
+            <span className="hover:underline cursor-pointer" onClick={() => navigate('/')}>Home</span> / <span>Blog</span>
+          </nav>
+        </div>
         <h1 className="text-4xl font-bold mb-6">Blog</h1>
         <p className="text-lg text-muted-foreground">
-          Welcome to my blog! Here, I share my thoughts, experiences, and insights on technology, software engineering, and more.
+          Hi, If you are seeing this, you probably know me, well, I'm Ben_G. This is where I share my journey, experiences, thoughts and insights.
+          I always like to write and share my thoughts with the world. Some might be about my life, some might be about my work.
+          For sure it might involve aliens, technology, short stories, Ai and some random stuff.
+          I hope you enjoy reading them as much as I enjoyed writing them, hope you find something useful here.
+          Feel free to find my medium account <a href="https://medium.com/@benjaminnechicattu" className="text-blue-500 hover:underline">here</a>.
         </p>
         <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {blogData.map((blog, index) => (
