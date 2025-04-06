7588335eca6086c9c4801c0f7d4df4454c358f41
Benjamin Nechicattu <benjaminnechicattu@gmail.com>
2025-04-06 18:14:13 +0530

    added more stories

--- src/data/blogs/blogs.tsx
+++ src/data/blogs/blogs.tsx
@@ -4,6 +4,8 @@ const blogs = [
   "003_ET_1.json",
   "004_education_1.json",
   "005_asgardia_malayalam_v1.json",
+  "006_pakalmanyan_v1.json",
+  "007_markidamushti_v2.json",
 ];
 
 export default blogs;
