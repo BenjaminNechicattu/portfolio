719cf900c5e9490bd909296f88f8d1e1e713eea1
Benjamin Nechicattu <benjaminnechicattu@gmail.com>
2025-04-06 17:55:26 +0530

    added old blogs

--- src/data/blogs/blogs.tsx
+++ src/data/blogs/blogs.tsx
@@ -1,6 +1,9 @@
 const blogs = [
   "001_aliens_1.json",
   "002_amayum_muyalum_1.json",
+  "003_ET_1.json",
+  "004_education_1.json",
+  "005_asgardia_malayalam_v1.json",
 ];
 
 export default blogs;
